05bed97a3814ccf2463dd9ebff3ff7ad229039b8
Miro Bucko <mbucko@hotmail.co.uk>
2024-07-21 14:44:33 -0400

    Fix openleetcodeui.sh script for Darwin.

--- src/ui/openleetcodeui.sh
+++ src/ui/openleetcodeui.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 run_exe() {
-    for D in OpenLeetCodeUI; do
-        if [ -f "${D}/OpenLeetCodeUI" ]; then
-            echo "Running OpenLeetCodeUI in ${D}"
-            "${D}/OpenLeetCodeUI" --problem_builds_dir=$(pwd)
+    if [[ "$(uname)" == "Darwin" ]]; then
+        app_path="./OpenLeetCodeUI/OpenLeetCodeUI.app/Contents/MacOS/OpenLeetCodeUI"
+        echo "app_path: $app_path"
+        if [ -x "$app_path" ]; then
+            echo "Running OpenLeetCodeUI"
+            "$app_path" --problem_builds_dir="$(pwd)"
             exit
+        else
+            echo "Error: OpenLeetCodeUI executable not found or not executable at $app_path"
         fi
-    done
-    echo "No OpenLeetCodeUI found in $(pwd)/OpenLeetCodeUI directory."
+    elif [[ "$(uname)" == "Linux" ]]; then
+        for D in OpenLeetCodeUI; do
+            if [ -f "${D}/OpenLeetCodeUI" ]; then
+                echo "Running OpenLeetCodeUI in ${D}"
+                "${D}/OpenLeetCodeUI.app" --problem_builds_dir=$(pwd)
+                exit
+            fi
+        done
+        echo "No OpenLeetCodeUI found in $(pwd)/OpenLeetCodeUI directory."
+    else
+        echo "Only Mac and Linux are supported."
+    fi
 }
 
 pushd "$(dirname "$0")" > /dev/null
