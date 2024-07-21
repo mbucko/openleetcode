37dca2bbac0d15f1d9aafdeb2e8f73884d388876
Miro Bucko <7475970+mbucko@users.noreply.github.com>
2024-07-21 14:45:07 -0400

    Fix clang builds.

--- data/languages/cpp/singletreenode.h
+++ data/languages/cpp/singletreenode.h
@@ -3,7 +3,9 @@
 
 #include "treenode.h"
 
-struct SingleTreeNode : public TreeNode { };
+struct SingleTreeNode : public TreeNode {
+    using TreeNode::TreeNode;
+};
 
 static_assert(sizeof(SingleTreeNode) == sizeof(TreeNode),
               "SingleTreeNode is not the same size as TreeNode");
